56a035d6f3725bb81c22c76fa307cf152733ffa1
xing.chen <caratcaus@gmail.com>
2026-03-27 18:01:01 +0800

    docs: 更新Day1

--- src/main/java/g0701_0800/s0724_find_pivot_index/Solution.java
+++ src/main/java/g0701_0800/s0724_find_pivot_index/Solution.java
@@ -1,23 +1,22 @@
 package g0701_0800.s0724_find_pivot_index;
-
 // #Easy #Array #Prefix_Sum #LeetCode_75_Prefix_Sum #Level_1_Day_1_Prefix_Sum
 // #2022_03_24_Time_2_ms_(69.67%)_Space_52.1_MB_(59.19%)
 
 public class Solution {
+
     public int pivotIndex(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        int[] sums = new int[nums.length];
-        sums[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sums[i] = sums[i - 1] + nums[i];
-        }
+        int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            int temp = sums[nums.length - 1] - sums[i];
-            if (i == 0 && 0 == temp || (i > 0 && sums[i - 1] == temp)) {
+            sum += nums[i];
+        }
+        int left = 0;
+        int right = sum;
+        for (int i = 0; i < nums.length; i++) {
+            right = right - nums[i];
+            if (left == right) {
                 return i;
             }
+            left += nums[i];
         }
         return -1;
     }
